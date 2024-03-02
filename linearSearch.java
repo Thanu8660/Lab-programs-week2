89c03da3c53ea6df8c108e533e47400efeec9705
Thanu8660 <161604050+Thanu8660@users.noreply.github.com>
2024-03-02 20:11:58 +0530

    Program 7 : Learn and Understand creating one-dimensional arrays, initializing and accessing the array elements. Implement a                        program to perform Sequential search in an array of N elements. ( use of break construct )

--- linearSearch.java
+++ linearSearch.java
@@ -1,10 +1,33 @@
 package Week05;
 
-public class linearSearch {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    import java.util.Scanner;
+	public class linearSearch
+	{
+		public static void main(String args[])
+		{
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter array size");
+			int n=sc.nextInt();
+			int[] array=new int[n];
+			System.out.println("enter the array elements");
+			for(int i=0;i<n;i++)
+			{
+				array[i]=sc.nextInt();	
+			}
+			System.out.println("enter the element to search");
+			int searchElement=sc.nextInt();
+			boolean found=false;
+			for(int i=0;i<n;i++)
+			{
+				if(array[i]==searchElement)
+				{
+					System.out.println("element found at index:"+i);
+					found=true;
+					break;	
+				}
+			}
+			
+		}
 
 	}
-
-}
+	
