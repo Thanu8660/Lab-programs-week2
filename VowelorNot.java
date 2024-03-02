b0b069cc2df873d1ada26e684f678a627e9501ab
Thanu8660 <161604050+Thanu8660@users.noreply.github.com>
2024-03-02 19:05:08 +0530

    Program 3 : Learn and Understand switch construct, demonstrate the same to check whether given character is Vowel or Not?

---
+++ VowelorNot.java
@@ -0,0 +1,28 @@
+package Week05;
+import java.util.Scanner;
+public class VowelorNot {
+	 public static void main(String[] args) {
+		 boolean isVowel=false;
+	     System.out.println("Enter a character/ alphabet:");
+	     Scanner sc=new Scanner(System.in);
+		 char ch=sc.next().charAt(0);
+		 switch(ch)
+		 {
+		 case'A':
+		 case'E':
+		 case'I':
+		 case'O':
+		 case'U':
+		 case'a':
+		 case'e':
+		 case'i':
+		 case'o':
+		 case'u':isVowel=true;
+		 }
+         if(isVowel==true)
+         System.out.println(ch+"is an vowel");
+         else
+         System.out.println(ch+"is a consonant");
+	 }
+}
+
