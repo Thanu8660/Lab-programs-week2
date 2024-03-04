f160285849820f8c835c8f8288dee265f60a38f4
Thanu8660 <161604050+Thanu8660@users.noreply.github.com>
2024-03-04 20:05:14 +0530

    Program 1:  Learn and Understand scope of variables, Demonstrate it with an suitable example.

---
+++ Hospital.java
@@ -0,0 +1,30 @@
+package Week02;
+class Hospital {
+	//Instance variable
+	String patientName;
+	int patientid;
+	//Static(class) variable
+	static String DocName= "Dr.Murthy";
+	void sethospital(String pN,int pId)
+	{
+	patientName=pN;
+	patientid=pId;
+	}
+	String getpatientName()
+	{
+	return patientName;
+	}
+	int getpatientid()
+	{ 
+		return patientid;
+	}
+}
+public class DemoScopeofVariables {
+
+	public static void main(String[] args) {
+		String hospitalName="Apollo Hosapital";
+		Hospital patient1=new Hospital();
+		patient1.sethospital("Anita Joseph",101);
+		System.out.println("The patient " +patient1.getpatientName()+" With the Id " +patient1.getpatientid()+" is treated by " +patient1.DocName+"at " +hospitalName);
+	}
+}
